16bf6fb2fb410c9c1a50880406ae1fbf8b4e50c8
GuiRodriguero <gui.rodriguero@hotmail.com>
2024-05-15 21:14:54 -0300

    Inicio do projeto

---
+++ hexburger/src/main/java/br/com/hexburger/HexburgerApplication.java
@@ -0,0 +1,13 @@
+package br.com.hexburger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexburgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexburgerApplication.class, args);
+	}
+
+}
---
+++ hexburger/src/test/java/br/com/hexburger/HexburgerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hexburger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexburgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
